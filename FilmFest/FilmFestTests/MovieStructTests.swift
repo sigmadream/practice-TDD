dde8204a60630479916a54cb22f44072efaaa087
Sangkon Han <sigmadream@gmail.com>
2018-09-02 01:40:33 +0900

    Added 테스트코드 작성(실패)

---
+++ FilmFest/FilmFestTests/MovieStructTests.swift
@@ -0,0 +1,28 @@
+//
+//  MovieStructTests.swift
+//  FilmFestTests
+//
+//  Created by Sangkon Han on 02/09/2018.
+//  Copyright © 2018 Sigmadream. All rights reserved.
+//
+
+import XCTest
+
+@testable import FilmFest
+class MovieStructTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testInit_MovieWithTitle() {
+        let testMovie = Movie(title: "Generic Blockbuster")
+    }
+    
+}
