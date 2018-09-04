fb57233727c7b326e1379e173ee85a9c7dc42bc1
Sangkon Han <sigmadream@gmail.com>
2018-09-04 20:42:37 +0900

    Added 4장. 프라이버시

--- CsharpTDD/CsharpTDD/CucurrencyTest.cs
+++ CsharpTDD/CsharpTDD/CucurrencyTest.cs
@@ -9,10 +9,8 @@ namespace CsharpTDD
         public void TestMultiplication() 
         {
             Dollar five = new Dollar(5);
-            Dollar product = five.Times(2);
-            Assert.Equal(10, product.amount);
-            product = five.Times(3);
-            Assert.Equal(15, product.amount);
+            Assert.Equal(new Dollar(10), five.Times(2));
+            Assert.Equal(new Dollar(15), five.Times(3));
         }
 
         [Fact]
--- CsharpTDD/README.md
+++ CsharpTDD/README.md
@@ -3,8 +3,10 @@
 ## Task List
 - [ ] $5 + 10CHF = 20
 - [X] $5 * 2 = $10
-- [ ] amount를 private 만들기
+- [X] amount를 private 만들기
 - [X] Dollar 부작용(side effect?)
 - [ ] Mony 반올림
 - [X] equals()
 - [ ] hashCode()
+- [ ] equal null
+- [ ] equal object
