aa1a58f5b24573a80b5252f025e5ce093ffce29d
Sangkon Han <sigmadream@gmail.com>
2018-09-04 19:49:24 +0900

    Added 2장. 타락한 객체

--- CsharpTDD/CsharpTDD/CucurrencyTest.cs
+++ CsharpTDD/CsharpTDD/CucurrencyTest.cs
@@ -9,8 +9,11 @@ namespace CsharpTDD
         public void TestMultiplication() 
         {
             Dollar five = new Dollar(5);
-            five.Times(2);
-            Assert.Equal(10, five.amount);
+            Dollar product = five.Times(2);
+            Assert.Equal(10, product.amount);
+            product = five.Times(3);
+            Assert.Equal(15, product.amount);
+
         }
     }
 }
--- CsharpTDD/README.md
+++ CsharpTDD/README.md
@@ -1,8 +1,8 @@
 # TDD with `C#`
 
-## TODO
+## Task List
 - [ ] $5 + 10CHF = 20
 - [X] $5 * 2 = $10
 - [ ] amount를 private 만들기
-- [ ] Dollar 부작용(side effect?)
+- [X] Dollar 부작용(side effect?)
 - [ ] Mony 반올림
